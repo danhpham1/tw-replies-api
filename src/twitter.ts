265a56b28a41ceef2082505d362736750387f285
danhpham <danhpham511998@gmail.com>
2025-09-28 19:36:05 +0700

    update

--- src/twitter.ts
+++ src/twitter.ts
@@ -42,7 +42,7 @@ export async function fetchReplyUsernamesForUrl(url: string, client: TwitterOpen
 
   do {
     // Random delay 10s to mitigate rate limits/bans
-    await sleep(15000);
+    await sleep(7000);
 
     const params: any = {
       focalTweetId: tweetId,
