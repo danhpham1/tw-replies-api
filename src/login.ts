878989955394ccdc00cb72560b866f930d2a3c1c
danhpham <danhpham511998@gmail.com>
2025-11-05 12:48:47 +0700

    fix

--- src/login.ts
+++ src/login.ts
@@ -2,6 +2,7 @@ import './polyfills/crypto';
 import { TwitterOpenApi, TwitterOpenApiClient } from 'twitter-openapi-typescript';
 import * as dotenv from 'dotenv';
 import { Account } from './accounts';
+import { HttpProxyAgent } from 'http-proxy-agent';
 
 dotenv.config();
 
@@ -17,6 +18,9 @@ export async function getClientForAccount(account: Account): Promise<TwitterOpen
   const existing = clientsCache.get(account.id);
   if (existing) return existing;
   const client = await createClientFromAccount(account);
+  const proxyUrl = `http://mhkudblk:rpy7qc3z8m9f@192.210.191.95:6081`
+  const agent = new HttpProxyAgent(proxyUrl);
+  client.getDefaultApi().initOverrides({ agent });
   clientsCache.set(account.id, client);
   return client;
 }
